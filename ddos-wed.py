cd43022aaafb1da062ded41e069c908f588b1adc
SatoshiCoder <100981673+SatoshiCoder@users.noreply.github.com>
2022-09-26 11:13:31 +0700

    Create ddos-wed.py

---
+++ ddos-wed.py
@@ -0,0 +1,15 @@
+from concurrent.futures import thread
+from colorama import Fore, Back, Style
+
+print(Fore.RED + "𝙌𝙪𝙖̂𝙣 𝙎𝙖𝙙 𝘽𝙤𝙮")
+print("TikTok:𝙌𝙪𝙖̂𝙣 𝙎𝙖𝙙 𝘽𝙤𝙮")
+print("Facebook:Đỗ Hồng Quân")
+print("-----------------------------------------------------------------------------------------------------")
+print("Tool Ddos Wed")
+http = input('Link wed:')
+thread = input('Thread:')
+time = input('Time(s):')
+
+print(Fore.RED + http)
+for i in range(10000):
+    print ("Wed sập rồi nha!")
